4ffdaf1bbc4fff0ab151d27d86d0d9c3d1eefeef
Kevin Jacobs <kevin91nl@gmail.com>
2018-03-26 16:07:07 +0200

    Added the EntitySalienceLoader for loading entity examples from the JSON files and added the corresponding tests.

--- dataset/loader.py
+++ dataset/loader.py
@@ -6,13 +6,49 @@ from chainer.dataset import DatasetMixin
 from tqdm import tqdm
 
 from dataset.validator import is_valid_esd_json
+from model.relevant_sentence import RSD
 from util.text import compute_sentence_similarity
+
+
+def balance_dataset(dataset: list, key: str, seed: int = 0) -> list:
+    """Balance a dataset.
+
+    Parameters
+    ----------
+    dataset : list
+        List of examples to balance (each example must have a field which is specified in the "key" argument).
+
+    key : str
+        The field (binary field consisting of True and False values) to balance for.
+
+    seed : int, optional
+        The seed to use for shuffling (default: 0).
+
+    Returns
+    -------
+    The balanced dataset.
+    """
+    np.random.seed(seed)
+    pos_examples = [example for example in dataset if example[key]]
+    neg_examples = [example for example in dataset if not example[key]]
+    min_class_size = min(len(pos_examples), len(neg_examples))
+
+    # Make sure that the examples per class are not more than the minimum class size
+    pos_examples = np.random.choice(pos_examples, min_class_size)
+    neg_examples = np.random.choice(neg_examples, min_class_size)
+
+    dataset = []
+    dataset.extend(pos_examples)
+    dataset.extend(neg_examples)
+    np.random.shuffle(dataset)
+
+    return dataset
 
 
 class RelevantSentencesLoader(DatasetMixin):
 
     def __init__(self, path: str, sent_tokenize: callable, sent_to_features: callable, balance: bool = False,
-                 seed: int = 0, max_files: int=-1):
+                 seed: int = 0, max_files: int = -1):
         """The file loader for the dataset files as described in the README.
 
         Parameters
@@ -74,19 +110,7 @@ class RelevantSentencesLoader(DatasetMixin):
                     self.dataset.append(example)
 
         if balance:
-            np.random.seed(seed)
-            pos_examples = [example for example in self.dataset if example['is_relevant']]
-            neg_examples = [example for example in self.dataset if not example['is_relevant']]
-            min_class_size = min(len(pos_examples), len(neg_examples))
-
-            # Make sure that the examples per class are not more than the minimum class size
-            pos_examples = np.random.choice(pos_examples, min_class_size)
-            neg_examples = np.random.choice(neg_examples, min_class_size)
-
-            self.dataset = []
-            self.dataset.extend(pos_examples)
-            self.dataset.extend(neg_examples)
-            np.random.shuffle(self.dataset)
+            self.dataset = balance_dataset(self.dataset, 'is_relevant', seed)
 
     @staticmethod
     def _compute_relevant_indices(text_sentences: list, abstract_sentences: list) -> set:
@@ -116,6 +140,128 @@ class RelevantSentencesLoader(DatasetMixin):
                 if score > 0.6:
                     relevant_indices.add(index)
         return relevant_indices
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def get_example(self, i: int) -> dict:
+        return self.dataset[i]
+
+
+class EntitySalienceLoader(DatasetMixin):
+
+    def __init__(self, path: str, sent_tokenize: callable, sent_to_features: callable, rsd_model: RSD = None,
+                 rsd_threshold: float = 0.0, rsd_sent_to_features: callable = None, balance: bool = False,
+                 seed: int = 0, max_files: int = -1, remove_duplicate_sentences: bool = True):
+        """The file loader for the dataset files for the entity salience task.
+
+        Parameters
+        ----------
+        path : str
+            Path to the folder containing the JSON files.
+
+        sent_tokenize : callable
+            A method which takes a document (text) as input and produces a list of sentences found in the document as
+            output.
+
+        sent_to_features : callable
+            A method which takes a sentence as input and produces features (a vector of embedding indices) as output.
+
+        rsd_model : RSD, optional
+            A trained RSD model (default: None).
+
+        rsd_threshold : float, optional
+            The minimum relevance score required for a sentence (predicted by the RSD model). Sentences with a lower
+            predicted relevance score are omitted (default: 0.0, allows all sentences).
+
+        rsd_sent_to_features : callable, optional
+            The sentence-to-features method used for the RSD model (default: None, required when rsd_model is not None).
+
+        balance : bool, optional
+            Whether to balance the dataset or not (default: False).
+
+        seed : int, optional
+            Seed used for shuffling during balancing (only used when balance=True, default: 0).
+
+        max_files : int, optional
+            The maximum number of files used, -1 for no maximum (default: -1).
+
+        remove_duplicate_sentences : bool, optional
+            When True, duplicate sentences are removed from the input (default: True).
+
+        Raises
+        ------
+        ValueError
+            When the path is not a valid directory.
+        ValueError
+            When the rsd_model is specified, but the rsd_sent_to_features method is not specified.
+        """
+        if not os.path.isdir(path):
+            raise IOError('The path "%s" is not a directory.' % path)
+        files = os.listdir(path)
+        if max_files != -1:
+            files = files[:max_files]
+
+        # Create a list containing the dataset
+        self.dataset = []
+
+        progressbar = tqdm(files)
+        for file in progressbar:
+            progressbar.set_description(file)
+            file_path = os.path.join(path, file)
+            with open(file_path, 'r') as input_file:
+                file_data = json.load(input_file)
+                text, entities = file_data['text'], file_data['entities']
+                is_valid_esd_json(file_data, is_train_document=True)
+                text_sentences = sent_tokenize(text)
+                if remove_duplicate_sentences:
+                    text_sentences = EntitySalienceLoader._remove_duplicate_sentences(text_sentences)
+
+                # Now filter out sentences using the RSD model
+                if rsd_model is not None:
+                    if rsd_sent_to_features is None:
+                        raise ValueError(
+                            'The rsd_model is specified, but the rsd_sent_to_features method is not specified.')
+                    scores = rsd_model.predict([rsd_sent_to_features(sentence) for sentence in text_sentences]).data
+                    scores = scores[:, 1]
+                    text_sentences = [sentence for sentence, score in zip(text_sentences, scores) if
+                                      score >= rsd_threshold]
+
+                # Skip if no sentence were found
+                if len(text_sentences) == 0:
+                    continue
+
+                for entity in entities:
+                    example = {
+                        'entity': entity,
+                        'sentences': text_sentences,
+                        'features': [sent_to_features(sentence) for sentence in text_sentences],
+                        'is_salient': entity['salience']
+                    }
+                    self.dataset.append(example)
+
+        if balance:
+            self.dataset = balance_dataset(self.dataset, 'is_salient', seed)
+
+    @staticmethod
+    def _remove_duplicate_sentences(sentences: list) -> list:
+        """Remove duplicate sentences.
+
+        Parameters
+        ----------
+        sentences : list
+            List of sentences (strings).
+
+        Returns
+        -------
+        List of sentences such that the first occurrence of each unique sentence is ordered the same way as in the input
+        data.
+        """
+        unique_sentences = []
+        for sentence in sentences:
+            if sentence.lower().strip() not in [sentence.lower().strip() for sentence in unique_sentences]:
+                unique_sentences.append(sentence)
+        return unique_sentences
 
     def __len__(self) -> int:
         return len(self.dataset)
--- test/test_dataset_loader.py
+++ test/test_dataset_loader.py
@@ -3,7 +3,9 @@ import unittest
 
 import nltk
 
-from dataset.loader import RelevantSentencesLoader
+from dataset.loader import RelevantSentencesLoader, EntitySalienceLoader
+from model.relevant_sentence import RSD
+from util.text import word_to_hash
 
 
 class TestLoaders(unittest.TestCase):
@@ -15,17 +17,21 @@ class TestLoaders(unittest.TestCase):
         assert os.path.exists(self.resources_path) and os.path.isdir(self.resources_path)
         assert os.path.exists(self.json_path) and os.path.isdir(self.json_path)
 
-    def test_nonexisting_path(self):
-        with self.assertRaises(IOError):
-            RelevantSentencesLoader(os.path.join(self.resources_path, 'nonexisting_directory'), nltk.sent_tokenize,
-                                    lambda sent: sent)
-
 
 class TestRelevantSentencesLoader(TestLoaders):
 
     def setUp(self):
         super(TestRelevantSentencesLoader, self).setUp()
         nltk.download('punkt')
+
+    def test_nonexisting_path(self):
+        with self.assertRaises(IOError):
+            RelevantSentencesLoader(os.path.join(self.resources_path, 'nonexisting_directory'), nltk.sent_tokenize,
+                                    lambda sent: sent)
+
+    def test_max_files(self):
+        loader = RelevantSentencesLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent, max_files=0)
+        self.assertEqual(0, loader.__len__())
 
     def test_example_structure(self):
         loader = RelevantSentencesLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent, balance=False)
@@ -56,6 +62,51 @@ class TestRelevantSentencesLoader(TestLoaders):
         self.assertEqual(pos_examples, neg_examples, 'With balance=True, the number of positive examples should equal '
                                                      'the number of negative examples.')
 
+
+class TestEntitySalienceLoader(TestLoaders):
+
+    def setUp(self):
+        super(TestEntitySalienceLoader, self).setUp()
+        nltk.download('punkt')
+        self.model = EntitySalienceLoader
+
+    def test_nonexisting_path(self):
+        with self.assertRaises(IOError):
+            EntitySalienceLoader(os.path.join(self.resources_path, 'nonexisting_directory'), nltk.sent_tokenize,
+                                 lambda sent: sent)
+
     def test_max_files(self):
-        loader = RelevantSentencesLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent, max_files=0)
+        loader = EntitySalienceLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent, max_files=0)
         self.assertEqual(0, loader.__len__())
+
+    def test_example_structure(self):
+        loader = EntitySalienceLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent, balance=True)
+        # There should be at least one example
+        self.assertGreater(loader.__len__(), 0, 'There should be at least one example.')
+        for i in range(loader.__len__()):
+            example = loader.get_example(i)
+            # Test the structure of the example (this should be tested at least once by one of the first assumptions)
+            self.assertTrue('entity' in example, 'The example should contain an "entity" field.')
+            self.assertTrue('sentences' in example, 'The example should contain a "sentences" field.')
+            self.assertTrue('features' in example, 'The example should contain a "features" field.')
+            self.assertTrue('is_salient' in example, 'The example should contain an "is_salient" field.')
+            self.assertTrue(type(example['is_salient']) == bool, 'The "is_salient" field should be a boolean.')
+
+    @staticmethod
+    def _sentence_to_features(sentence):
+        return [word_to_hash(word, 10) for word in nltk.word_tokenize(sentence)]
+
+    def test_rsd_model(self):
+        model = RSD(10, 8, 8, 0.5)
+        EntitySalienceLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent,
+                             rsd_model=model,
+                             rsd_sent_to_features=TestEntitySalienceLoader._sentence_to_features,
+                             rsd_threshold=0.5)
+
+    def test_rsd_model_no_method_specified(self):
+        model = RSD(8, 8, 8, 0.5)
+        with self.assertRaises(ValueError):
+            EntitySalienceLoader(self.json_path, nltk.sent_tokenize, lambda sent: sent,
+                                 rsd_model=model,
+                                 rsd_sent_to_features=None,
+                                 rsd_threshold=0.5)
